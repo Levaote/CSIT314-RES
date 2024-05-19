c2d7b2a3e7d65d3f7ba9805a94f9801167ae379b
darylstephen <darylsteph98@gmail.com>
2024-05-19 18:33:48 +0800

    Update RegistrationController.php
    URL transfer was corrected from login.php to index.php

--- RegistrationController.php
+++ RegistrationController.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         $sql_profile = "INSERT INTO UserProfiles (user_id, first_name, last_name, address, profile_picture, bio) VALUES ('$user_id', '$first_name', '$last_name', '$address', '$profile_picture', '$bio')";
         if ($conn->query($sql_profile) === TRUE) {
-            header("Location: login.php");
+            header("Location: index.php");
             exit;
         } else {
             echo "Error: " . $sql_profile . "<br>" . $conn->error;
